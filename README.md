3ef34eb60158ea87c57e98c0e517fca4f4fb3ced
zjc <15118850470>
2017-09-20 01:04:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# spring-boot-starter-transaction
+* [spring-boot-starter-transaction-tcc(二阶段提交TCC分布式事务)]()
+* [spring-boot-starter-transaction-ons(ONS分布式消息事务)]()
---
+++ spring-boot-starter-transaction-ons-console/src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author throwable
+ * @version v1.0
+ * @description
+ * @since 2017/9/20 1:02
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
